882dbf06f2de885f8cec73908343151a0940b6db
cc2228 <cc2228@cornell.edu>
2016-04-28 22:15:49 -0400

    deleted stuff

--- js/main.js
+++ js/main.js
@@ -2,9 +2,5 @@ $(document).ready(function() {
 	$("button").click( function () {
 		$(this).addClass('active').siblings().removeClass('active');
 		type = (this.id);
-		// console.log(type);
-		// var year = $("#slider2").slider("option", "value");
-		// console.log(year);
-		// loadChords(year, type);
 	})
 })
